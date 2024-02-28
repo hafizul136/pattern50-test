cd0332e83dd2c3c98e84bfb942f850940777a464
Hafiz <hafizul@6sensehq.com>
2024-02-28 15:02:13 +0600

    conflict resolved

--- src/modules/technology-category/technology-category.module.ts
+++ src/modules/technology-category/technology-category.module.ts
@@ -2,8 +2,8 @@ import { AuthService } from '@modules/auth/auth.service';
 import { Permission, PermissionSchema } from '@modules/permissions/entities/permission.entity';
 import { PermissionsService } from '@modules/permissions/permissions.service';
 import { RolePermission, RolePermissionSchema } from '@modules/role-permission/entities/role-permission.entity';
-import { Role, RoleSchema } from '@modules/roles/entities/role.entity';
-import { RolesService } from '@modules/roles/roles.service';
+import { Role, RoleSchema } from '@modules/role/entities/role.entity';
+import { RoleService } from '@modules/role/role.service';
 import { UserRole, UserRoleSchema } from '@modules/user-role/entities/user-role.entity';
 import { UserRoleService } from '@modules/user-role/user-role.service';
 import { User, UserSchema } from '@modules/users/entities/user.entity';
@@ -27,6 +27,6 @@ import { TechnologyCategoryService } from './technology-category.service';
     ])
   ],
   controllers: [TechnologyCategoryController],
-  providers: [TechnologyCategoryService, JwtService, UsersService, AuthService, UserRoleService, RolesService, PermissionsService],
+  providers: [TechnologyCategoryService, JwtService, UsersService, AuthService, UserRoleService, RoleService, PermissionsService],
 })
 export class TechnologyCategoryModule { }
