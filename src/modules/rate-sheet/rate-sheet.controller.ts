3805ada44e15a04ab990b105a87e2da668eeb7b5
dabasishJoy <dabasish@6sensehq.com>
2024-03-11 11:32:38 +0600

    P-50 create api to active/inactive ratesheet

--- src/modules/rate-sheet/rate-sheet.controller.ts
+++ src/modules/rate-sheet/rate-sheet.controller.ts
@@ -23,12 +23,13 @@ export class RateSheetController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.rateSheetService.findOne(+id);
+    return this.rateSheetService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRateSheetDto: UpdateRateSheetDto) {
-    return this.rateSheetService.update(+id, updateRateSheetDto);
+  @Patch(':id/status')
+  @Permissions('company.create')
+  update(@Param('id') id: string, @Body() updateRateSheetDto: UpdateRateSheetDto, @GetUser() user: IUser) {
+    return this.rateSheetService.update(id, updateRateSheetDto, user);
   }
 
   @Delete(':id')
